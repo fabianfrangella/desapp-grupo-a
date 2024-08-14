7c1cbaff148a7a49f39a64258a48c5a4f7dbf577
Fabian Frangella <fabianfrangella@gmail.com>
2024-08-14 19:45:17 -0300

    proyecto base

---
+++ crypto-exchange/src/test/java/com/unq/crypto_exchange/CryptoExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unq.crypto_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
