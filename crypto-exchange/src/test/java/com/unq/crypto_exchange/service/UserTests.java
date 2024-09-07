894bc80bf2f17d89486687f966214b19061aa1a6
Andres Mora <andeehr@gmail.com>
2024-09-07 20:54:32 -0300

    agrego test

---
+++ crypto-exchange/src/test/java/com/unq/crypto_exchange/service/UserTests.java
@@ -0,0 +1,59 @@
+package com.unq.crypto_exchange.service;
+
+import com.unq.crypto_exchange.domain.entity.CryptoUser;
+import com.unq.crypto_exchange.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UserTests {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    void registerUserSuccessfully() {
+        // Arrange
+        CryptoUser user = new CryptoUser();
+        user.setEmail("test@example.com");
+        user.setPassword("password");
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+
+        // Act
+        CryptoUser registeredUser = userService.register(user);
+
+        // Assert
+        assertNotNull(registeredUser);
+        verify(userRepository).save(user);
+    }
+
+
+    @Test
+    void registerUserAlreadyExists() {
+        // Arrange
+        CryptoUser user = new CryptoUser();
+        user.setEmail("test@example.com");
+        user.setPassword("password");
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+        // Act & Assert
+        assertThrows(UserAlreadyExistException.class, () -> userService.register(user));
+        verify(userRepository, never()).save(any());
+    }
+}
