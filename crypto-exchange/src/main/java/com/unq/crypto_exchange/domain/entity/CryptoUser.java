a690375fc3f2c70a445fd8e3b1cb934c1efee525
Andres Mora <andeehr@gmail.com>
2024-09-09 22:13:06 -0300

    Feature/modelo (#2)

--- crypto-exchange/src/main/java/com/unq/crypto_exchange/domain/entity/CryptoUser.java
+++ crypto-exchange/src/main/java/com/unq/crypto_exchange/domain/entity/CryptoUser.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,22 +18,37 @@ public class CryptoUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NonNull
     @Size(min = 3, max = 30)
     private String name;
+
     @NonNull
     @Size(min = 3, max = 30)
     private String lastName;
+
     @NonNull
     @Column(unique = true)
     @Pattern(regexp=".+@.+\\.[a-z]+", message="Invalid email address!")
     private String email;
+
     @NonNull
     private String password;
+
     @NonNull
     @Size(min = 22, max = 22)
     private String cvu;
+
     @NonNull
     @Size(min = 8, max = 8)
     private String cryptoWalletAddress;
+
+    @Builder.Default
+    @NonNull
+    private Integer reputation = 0;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Transaction> transactions = new HashSet<>();
+
 }
