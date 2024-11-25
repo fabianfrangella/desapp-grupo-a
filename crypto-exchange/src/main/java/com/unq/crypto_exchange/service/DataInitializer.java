da6faa96223e3e72c51775b24486ca7269dd9839
Fabian Frangella <fabianfrangella@gmail.com>
2024-11-24 22:42:27 -0300

    agrega initializer

---
+++ crypto-exchange/src/main/java/com/unq/crypto_exchange/service/DataInitializer.java
@@ -0,0 +1,34 @@
+package com.unq.crypto_exchange.service;
+
+import com.unq.crypto_exchange.domain.entity.CryptoUser;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DataInitializer {
+
+    private final UserService userService;
+
+    @PostConstruct
+    public void initialize() {
+        userService.register(CryptoUser.builder()
+                        .cryptoWalletAddress("12345678")
+                        .email("fabian@test.com")
+                        .password("1234568#aA")
+                        .name("Fabian")
+                        .lastName("Frangella")
+                        .cvu("1234567891123456789112")
+                .build());
+
+        userService.register(CryptoUser.builder()
+                .cryptoWalletAddress("12345679")
+                .email("andres@test.com")
+                .password("1234568#aA")
+                .name("Andres")
+                .lastName("Mora")
+                .cvu("1234567891123456789113")
+                .build());
+    }
+}
