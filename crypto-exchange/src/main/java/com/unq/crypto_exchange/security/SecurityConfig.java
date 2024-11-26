c26e912a2838be96b56d7faa346b8ba4d0873912
Andres Mora <andeehr@gmail.com>
2024-11-25 21:01:01 -0300

    fix prometheus

--- crypto-exchange/src/main/java/com/unq/crypto_exchange/security/SecurityConfig.java
+++ crypto-exchange/src/main/java/com/unq/crypto_exchange/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/user/**", "/h2-console/**")
+                        .requestMatchers("/user/**", "/h2-console/**", "/actuator/prometheus")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
